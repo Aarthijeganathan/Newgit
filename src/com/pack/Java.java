2aef399fb86bf32cbdf85117be82affccb6c2fe7
Aarthijeganathan <aarthijegan94@gmail.com>
2022-01-02 19:40:41 +0530

    aarthi changes commited

--- src/com/pack/Java.java
+++ src/com/pack/Java.java
@@ -2,10 +2,9 @@ package com.pack;
 
 public class Java {
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
-		int c=a+b;
-		System.out.println(c);
+		String a="aarthi";
+		String b="selenium";
+		System.out.println(a+" "+b);
 	}
 
 }
